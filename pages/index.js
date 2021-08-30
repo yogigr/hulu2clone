811128ee5df8ce6216957fa008d28a6ada605fee
Yogi Gilang Ramadhan <yogigilang182@gmail.com>
2021-08-30 19:41:26 +0700

    Building the Result Component

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Result from '../components/Result'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
   return (
     <div>
       <Head>
@@ -19,7 +21,24 @@ export default function Home() {
       <Nav />
 
       {/* result */}
+      <Result results={results} />
 
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch (
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
